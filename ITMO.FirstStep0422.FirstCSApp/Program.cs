ed03fad5574e0b1eb2a467b910fced46983455d0
SVTsvetkov <tsvetkov71@gmail.com>
2022-04-12 13:10:13 +0300

    ver1

--- ITMO.FirstStep0422.FirstCSApp/Program.cs
+++ ITMO.FirstStep0422.FirstCSApp/Program.cs
@@ -10,57 +10,74 @@ namespace ITMO.FirstStep0422.FirstCSApp
     {
         static void Main(string[] args)
         {
-            string answer = "да";
+            string answer = "y";
             do
             {
-                Console.WriteLine("Привет, ты профессиональный лыжник?");
+                Console.WriteLine("Hi, are you a professional skier? (y/n)");
                 string result = Console.ReadLine();
 
-                if (result == "да")
+                if (result == "y")
                 {
-                    Console.WriteLine("Похвально! Введи внешнюю температуру");
-                    string temp = Console.ReadLine();
-                    switch (temp)
+                    Console.WriteLine("Great! Enter the outdoor temperature");
+                    string outdoor = Console.ReadLine();
+
+                    if (!int.TryParse(outdoor, out int temp))
                     {
-                        case "-20":
-                            Console.WriteLine("Используй тип мази VR25 для спортсменов");
+                        Console.WriteLine("Not a number");
+                    }
+                    else
+                    {
+                        switch (temp)
+                        {
+                        case int n when (n >= -20 && n < -8):
+                            Console.WriteLine("Use the VR25 type for athletes");
                             break;
-                        case "-10":
-                            Console.WriteLine("Используй тип мази VR35 для спортсменов");
+                        case int n when (n >= -7 && n < -2):
+                            Console.WriteLine("Use the VR35 type for athletes");
                             break;
-                        case "0":
-                            Console.WriteLine("Используй тип мази VR45 для спортсменов");
+                        case int n when (n >= -2 && n < 3):
+                            Console.WriteLine("Use the VR45 type for athletes");
                             break;
                         default:
-                            Console.WriteLine("Не существует такого типа мази для указанной температуры");
+                            Console.WriteLine("There is no such type for the specified temperature");
                             break;
+                        }
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Всё впереди! А мы поможем! Введи внешнюю температуру");
-                    string temp = Console.ReadLine();
-                    switch (temp)
+                    Console.WriteLine("Everything is ahead! And we will help you! Enter the outdoor temperature");
+                    string outdoor = Console.ReadLine();
+
+                    if (!int.TryParse(outdoor, out int temp))
                     {
-                        case "-20":
-                            Console.WriteLine("Используй тип мази VR10 для любителей");
-                            break;
-                        case "-10":
-                            Console.WriteLine("Используй тип мази VR20 для любителей");
-                            break;
-                        case "0":
-                            Console.WriteLine("Используй тип мази VR40 для любителей");
-                            break;
-                        default:
-                            Console.WriteLine("Не существует такого типа мази для указанной температуры");
-                            break;
+                        Console.WriteLine("Not a number");
+                    }
+                    else
+                    {
+                        switch (temp)
+                        {
+                            case int n when (n >= -20 && n < -12):
+                                Console.WriteLine("Use the VR10 type for amateur skiers");
+                                break;
+                            case int n when (n >= -11 && n < -5):
+                                Console.WriteLine("Use the VR20 type for amateur skiers");
+                                break;
+                            case int n when (n >= -4 && n < 3):
+                                Console.WriteLine("Use the VR30 type for amateur skiers");
+                                break;
+                            default:
+                                Console.WriteLine("There is no such type for the specified temperature");
+                                break;
+                        }
                     }
                 }
-                Console.WriteLine("Повторить выбор?");
+                Console.WriteLine("Repeat selection? (y/n)");
                 answer = Console.ReadLine();
             }
-            while (answer == "да");
-            Console.WriteLine("Новых рекордов на лыжне в любую погоду !");
+            while (answer == "y");
+            Console.WriteLine("Enjoy and have a new records on the ski track in any weather!");
+
         }
     }
 }
